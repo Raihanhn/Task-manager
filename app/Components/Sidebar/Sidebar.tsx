d6bfca68e95e196d5bda3501870c450d111d2cff
Raihan <raihanhn20@gmail.com>
2024-01-01 23:03:04 +0600

    style added

--- app/Components/Sidebar/Sidebar.tsx
+++ app/Components/Sidebar/Sidebar.tsx
@@ -1,8 +1,23 @@
 "use client";
 import React from "react";
+import styled from "styled-components";
+import { useGlobalState } from "@/app/context/globalProvider";
 
-const Sidebar = () => {
-  return <div>Sidebar</div>;
-};
+function Sidebar() {
+  const { theme } = useGlobalState();
+
+  console.log(theme);
+
+  return <SidebarStyled theme={theme}>Sidebar</SidebarStyled>;
+}
+
+const SidebarStyled = styled.nav`
+  position: relative;
+  width: ${(props) => props.theme.sidebarWidth};
+  background-color: ${(props) => props.theme.colorBg2};
+  border: 2px solid ${(props) => props.theme.borderColor2};
+
+  border-radius: 1rem;
+`;
 
 export default Sidebar;
